21c67ff1b524c42fdc0ca769be5561b5d8f73dc9
Muhammad Yaseen <Yaseen@Muhammads-MacBook-Pro.local>
2024-05-24 11:36:21 +0500

    work

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -16,7 +16,22 @@ const Sidebar = () => {
       console.error("Error logging out:", error.message);
     }
   }
-    
+
+  const fetchBoards = () => {
+    fetch(`${API_URL}/boards/list/${userDetails?.uid}`)
+ .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setBoards(data?.boards);
+      })
+      .catch((error) => {
+        console.error("There was a problem fetching the boards:", error);
+      });
+  };
     const CreateBoardModal = ({ isOpen, onClose, onCreateBoard }) => {
         const [boardName, setBoardName] = useState('');
     
@@ -42,7 +57,7 @@ const Sidebar = () => {
                 .then(data => {
                 setSuccessMessage('Board created successfully!');
                 console.log('Board created successfully:', data);
-           
+               fetchBoards()
             })
             .catch(error => {
                 console.error('There was a problem creating the board:', error);
